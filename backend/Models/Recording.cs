bdf4212531fd9268f321f16620879a88a2304513
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-01-14 12:46:36 +0100

    Added description to the properties

--- backend/Models/Recording.cs
+++ backend/Models/Recording.cs
@@ -5,6 +5,7 @@ public class Recording
     public long Id { get; set; }
     public string Path { get; set; }
     public string Name { get; set; }
+    public string? Description { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
     public TimeOnly StartTime { get; set; }
