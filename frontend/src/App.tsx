345e102bd9e4f2442b70f8a71a5439086da16b7e
uomoazione <manuelcampi20@gmail.com>
2024-01-18 18:41:33 +0100

    commented Require Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { VideoList } from "./pages/VideoList";
 import "./App.css";
-import { RequireAuth } from "react-auth-kit";
+//import { RequireAuth } from "react-auth-kit";
 
 const Layout = () => (
   <>
