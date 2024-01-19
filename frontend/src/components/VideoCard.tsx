665629258369289baa876c7f4199ff6b52b5d09a
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-01-19 10:40:58 +0100

    Added props to the VideoCard component

--- frontend/src/components/VideoCard.tsx
+++ frontend/src/components/VideoCard.tsx
@@ -3,30 +3,40 @@
 import { DownloadButton } from "./DownloadButton";
 import { VideoPlayer } from "./VideoPlayer";
 
-export const VideoCard = () => {
+interface VideoCardProps {
+  id: number;
+  name: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  startTime: string;
+  endTime: string;
+  duration: string;
+  size: number;
+}
+
+export const VideoCard = (props: VideoCardProps) => {
   return (
     <>
-      <div className="max-w-lg Gelion rounded-md  overflow-hidden  shadow-2xl m-10">
+      <div className="min-w-lg Gelion rounded-md  overflow-hidden  shadow-2xl m-10">
         <VideoPlayer />
         <div className="px-6 py-4">
           <h1
             id="videoTitle"
             className="font-bold text-center text-xl mb-3 overflow-hidden text-ellipsis whitespace-nowrap"
           >
-            S20240113-000000-E20240113-0000
+            {props.name}
           </h1>
           <div className="flex lg:flex-row md:flex-col sm:flex-col justify-between">
             <p id="timeRange" className="text-gray-700 text-base mb-4">
-              🕒00:00:01~00:01:00
+              🕒{props.startTime}~{props.endTime}
             </p>
             <p id="dateRange" className="text-gray-700 text-base mb-4">
-              📅2024/01/10~2024/01/10
+              📅{props.startDate}~{props.endDate}
             </p>
           </div>
           <p id="videoDescription" className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+            {props.description}
           </p>
         </div>
         <DownloadButton />
