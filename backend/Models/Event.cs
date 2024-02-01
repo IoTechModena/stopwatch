7ab226bff1c5e14474d3867f70297a2ea677b528
YoussefEll <134938337+YoussefEll@users.noreply.github.com>
2024-02-01 16:48:44 +0100

    Create class Event

---
+++ backend/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace backend.Models
+{
+    public class Event
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDateTime { get; set; }
+        
+        [Required]
+        public DateTime EndDateTime { get; set; }
+        public Event()
+        {
+        }
+
+
+
+    }
+}
