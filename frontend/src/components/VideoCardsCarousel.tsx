6d0659e9c89f8840a74108fd3d608b046e6f0e16
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-02-02 08:08:20 +0100

    Improved carousel responsiveness

--- frontend/src/components/VideoCardsCarousel.tsx
+++ frontend/src/components/VideoCardsCarousel.tsx
@@ -14,29 +14,30 @@ export const VideoCardsCarousel = ({ children }: VideoCardsCarouselProps) => {
         <h1 className="text-3xl font-extrabold mt-10 mb-1">🔔Evento 1</h1>
         <p className="text-gray-700 font-bold">Numero di filmati: x</p>
         <div className="flex justify-center gap-10">
-          <p className="text-gray-500 font-normal">
+          <p className="text-gray-500 font-normal ">
             Inizio: 00:00:00 01/01/1970
           </p>
           <p className="text-gray-500 font-normal">Fine: 00:00:00 01/01/1970</p>
         </div>
       </div>
       <Carousel
-        className="mx-20"
+        className="lg:mx-20 md:mx-10 sm:mx-5  mb-5"
         arrows
+        slidesToSlide={1}
         draggable={false}
         showDots={true}
         responsive={{
           desktop: {
             breakpoint: {
               max: 3000,
-              min: 1024,
+              min: 1300,
             },
             items: 3,
             partialVisibilityGutter: 40,
           },
           mobile: {
             breakpoint: {
-              max: 464,
+              max: 860,
               min: 0,
             },
             items: 1,
@@ -44,8 +45,8 @@ export const VideoCardsCarousel = ({ children }: VideoCardsCarouselProps) => {
           },
           tablet: {
             breakpoint: {
-              max: 1024,
-              min: 464,
+              max: 1300,
+              min: 860,
             },
             items: 2,
             partialVisibilityGutter: 30,
