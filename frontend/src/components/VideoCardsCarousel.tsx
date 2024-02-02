dc5ecac9765a70abf6649e4387029513a0a839fd
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-02-02 08:25:10 +0100

    Refactored Event Header

--- frontend/src/components/VideoCardsCarousel.tsx
+++ frontend/src/components/VideoCardsCarousel.tsx
@@ -1,7 +1,7 @@
 import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
-
 import { ReactNode } from "react";
+import { EventHeader } from "./EventHeader";
 
 interface VideoCardsCarouselProps {
   children: ReactNode;
@@ -10,16 +10,12 @@ interface VideoCardsCarouselProps {
 export const VideoCardsCarousel = ({ children }: VideoCardsCarouselProps) => {
   return (
     <>
-      <div id="eventDescription" className="text-center Gelion">
-        <h1 className="text-3xl font-extrabold mt-10 mb-1">🔔Evento 1</h1>
-        <p className="text-gray-700 font-bold">Numero di filmati: x</p>
-        <div className="flex justify-center gap-10">
-          <p className="text-gray-500 font-normal ">
-            Inizio: 00:00:00 01/01/1970
-          </p>
-          <p className="text-gray-500 font-normal">Fine: 00:00:00 01/01/1970</p>
-        </div>
-      </div>
+      <EventHeader
+        id={0}
+        recordingCount={0}
+        startDateTime={"00:00:00 01/01/2000"}
+        endDateTime={"00:00:00 01/01/2000"}
+      />
       <Carousel
         className="lg:mx-20 md:mx-10 sm:mx-5  mb-5"
         arrows
@@ -58,5 +54,3 @@ export const VideoCardsCarousel = ({ children }: VideoCardsCarouselProps) => {
     </>
   );
 };
-
-export default Carousel;
