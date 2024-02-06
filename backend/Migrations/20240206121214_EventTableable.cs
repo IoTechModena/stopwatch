89f6fd043ffcb88df80be3ab1a6d509f48ed4b33
YoussefEll <134938337+YoussefEll@users.noreply.github.com>
2024-02-06 14:22:59 +0100

    add migration model event

---
+++ backend/Migrations/20240206121214_EventTableable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventTableable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "EventId",
+                table: "Recordings",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Channel = table.Column<byte>(type: "smallint", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StartDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recordings_EventId",
+                table: "Recordings",
+                column: "EventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recordings_Event_EventId",
+                table: "Recordings",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recordings_Event_EventId",
+                table: "Recordings");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recordings_EventId",
+                table: "Recordings");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Recordings");
+        }
+    }
+}
