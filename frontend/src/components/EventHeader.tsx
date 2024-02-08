103d09e043f5d491d0a1a7ed706eeb1c802ae3d3
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-02-08 18:03:38 +0100

    Improved EventHeader

--- frontend/src/components/EventHeader.tsx
+++ frontend/src/components/EventHeader.tsx
@@ -1,8 +1,11 @@
+import { formatDateTime } from "@/lib/utils";
+
 interface EventHeaderProps {
   id: number;
   recordingCount: number;
   startDateTime: string;
   endDateTime: string;
+  channel: number;
 }
 
 export const EventHeader = ({
@@ -10,17 +13,25 @@ export const EventHeader = ({
   recordingCount,
   startDateTime,
   endDateTime,
+  channel,
 }: EventHeaderProps) => {
   return (
     <>
       <div id="eventDescription" className="text-center Gelion">
-        <h1 className="text-3xl font-extrabold mt-10 mb-1">🔔Evento {id}</h1>
-        <p className="text-gray-700 font-bold">
-          Numero di filmati: {recordingCount}
-        </p>
+        <h1 className="text-3xl font-extrabold mb-1">🔔Evento {id}</h1>
         <div className="flex justify-center gap-10">
-          <p className="text-gray-500 font-normal ">Inizio: {startDateTime}</p>
-          <p className="text-gray-500 font-normal">Fine: {endDateTime}</p>
+          <p className="text-gray-700 font-bold">
+            Numero di filmati: {recordingCount}
+          </p>
+          <p className="text-gray-700 font-bold">Canale: {channel}</p>
+        </div>
+        <div className="flex justify-center gap-10">
+          <p className="text-gray-500 font-normal ">
+            Inizio: {formatDateTime(startDateTime)}
+          </p>
+          <p className="text-gray-500 font-normal">
+            Fine: {formatDateTime(endDateTime)}
+          </p>
         </div>
       </div>
     </>
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -17,3 +17,16 @@ export function formatBytes(bytes: number, decimals = 2) {
 
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
+
+export function formatDateTime(isoString: string) {
+  const date = new Date(isoString);
+  date.setUTCHours(date.getUTCHours()); // Increase the time by one hour to match the nvr TimeZone
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const hours = String(date.getUTCHours()).padStart(2, "0");
+  const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+  const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
