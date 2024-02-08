6ef55168142c6b48e218292e8490f87aea2160c2
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-02-08 18:03:09 +0100

    Updated fetched data to show events and their recordings

---
+++ frontend/src/pages/EventList.tsx
@@ -0,0 +1,98 @@
+//Author: Aboom
+import { useState, useEffect } from "react";
+import { Searchbox } from "../components/Searchbox";
+import { VideoCard } from "../components/VideoCard";
+import { VideoCardsCarousel } from "../components/VideoCardsCarousel";
+import { EventHeader } from "../components/EventHeader";
+import BeatLoader from "react-spinners/BeatLoader";
+import axios from "axios";
+import React from "react";
+
+interface Recording {
+  id: number;
+  path: string;
+  name: string;
+  description: string;
+  startDateTime: string;
+  endDateTime: string;
+  duration: string;
+  size: number;
+  eventId: number;
+}
+
+interface Event {
+  id: number;
+  recordings: Recording[];
+  channel: number;
+  name: string;
+  startDateTime: string;
+  endDateTime: string;
+}
+
+const getEvents = async () => {
+  try {
+    const response = await axios.get("http://localhost/api/getEvents");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const VideoList = () => {
+  const [eventList, setEventList] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const data = await getEvents();
+      setEventList(data);
+      setLoading(false);
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <>
+      <Searchbox datepickerIcon key="Searchbox" />
+      {eventList
+        .sort((a, b) => b.id - a.id)
+        .map((data: Event, index: number) => (
+          <React.Fragment key={`event-${data.id}`}>
+            <EventHeader
+              id={data.id}
+              recordingCount={data.recordings.length}
+              startDateTime={data.startDateTime}
+              endDateTime={data.endDateTime}
+              channel={data.channel}
+            />
+            <VideoCardsCarousel>
+              {data.recordings.map((recording) => (
+                <VideoCard
+                  key={`c-${data.id} recording-${recording.id}`}
+                  id={recording.id}
+                  description={
+                    recording.description === ""
+                      ? "Questo video non ha una descrizione."
+                      : recording.description
+                  }
+                  startDateTime={recording.startDateTime}
+                  endDateTime={recording.endDateTime}
+                  duration={recording.duration}
+                  size={recording.size}
+                  name={recording.name}
+                />
+              ))}
+            </VideoCardsCarousel>
+            {index !== eventList.length - 1 && (
+              <hr className="lg:mx-20 md:mx-10 sm:mx-5 mb-10 mt-10" />
+            )}
+          </React.Fragment>
+        ))}
+      <BeatLoader
+        className="text-center my-10"
+        color="#eab308"
+        loading={loading}
+      />
+    </>
+  );
+};
