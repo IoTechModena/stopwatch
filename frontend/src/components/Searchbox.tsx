2cd07c4965441296fc29b107221496132b194f02
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2024-03-07 18:00:00 +0100

    Revamped Searchbox channel logic

--- frontend/src/components/Searchbox.tsx
+++ frontend/src/components/Searchbox.tsx
@@ -1,24 +1,31 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { ChannelContext } from "@/context/ChannelContext";
 
-interface searchboxProps {
+interface SearchboxProps {
   searchInput: string;
   setSearchInput: (input: string) => void;
 }
-
-export const Searchbox = ({ searchInput, setSearchInput }: searchboxProps) => {
+// Ho modificato la const per renderla Typescript friendly, facciamolo sempre per evitare errori di compilazione
+export const Searchbox: React.FC<SearchboxProps> = ({
+  searchInput,
+  setSearchInput,
+}) => {
   const { selectedChannel, setSelectedChannel } = useContext(ChannelContext);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
   };
 
+  //ho sostituito il value stringa con number onde evitare una conversione
+  const handleSelectChange = (value: number) => {
+    setSelectedChannel(Number(value));
+    setIsDropdownOpen(false);
+  };
+
   return (
     <>
-      <form
-        className="my-12 p-2 Gelion"
-        onSubmit={handleSubmit}
-      >
+      <form className="my-12 p-2 Gelion" onSubmit={handleSubmit}>
         <div className="relative max-w-3xl mx-auto flex gap-2 sm:gap-6">
           <div className="absolute flex items-center ps-3 pointer-events-none">
             <i className="absolute fa fa-search text-black top-5 left-4"></i>
@@ -32,20 +39,34 @@ export const Searchbox = ({ searchInput, setSearchInput }: searchboxProps) => {
             placeholder="Che evento stai cercando?"
             name="search-box"
           />
-          <select
-            value={selectedChannel?.toString()}
-            onChange={(e) =>
-              setSelectedChannel(
-                e.target.value === "" ? null : Number(e.target.value)
-              )
-            }
-            id="channels"
-            className="shadow-xl text-center bg-gray-400 text-black text-sm rounded-lg block pl-2 focus:outline-none"
-          >
-            <option value="">Canale</option>
-            <option value="0">Canale: 0</option>
-            <option value="1">Canale: 1</option>
-          </select>
+
+          {/* Il select è stato sostituito da un dropdown */}
+          <div className="relative">
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="shadow-xl bg-gray-400 text-sm rounded-lg h-14 w-24 hover:bg-gray-300"
+            >
+              {selectedChannel !== undefined && selectedChannel !== null
+                ? `Canale: ${selectedChannel}`
+                : "Seleziona il canale"}
+            </button>
+            {isDropdownOpen && (
+              <ul className="absolute z-10 mt-2 w-full bg-gray-400 shadow-lg rounded-md text-center">
+                <li
+                  className="my-2 hover:bg-gray-300 rounded-lg py-1"
+                  onClick={() => handleSelectChange(0)}
+                >
+                  Canale: 0
+                </li>
+                <li
+                  className="mb-2 hover:bg-gray-300 rounded-md py-1"
+                  onClick={() => handleSelectChange(1)}
+                >
+                  Canale: 1
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </form>
     </>
