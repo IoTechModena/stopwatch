d07d47d8da1bf032849f683c70e1f703234bb342
YoussefEll <134938337+YoussefEll@users.noreply.github.com>
2024-03-13 17:52:44 +0100

    add error page

---
+++ frontend/src/components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from './Navbar';
+
+export default function Error() {
+    return (
+        <>
+         <Navbar />
+            <div className="errorContainer">
+                <img src="/imgs/OIG4.jpeg" alt="Error 404" className="centeredImage" />
+            </div>
+        </>
+    )
+}
